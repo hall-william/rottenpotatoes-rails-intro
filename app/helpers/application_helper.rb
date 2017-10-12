42934d0f04522bb21a140d9bc168d64a463e73a4
William Hall <whall20@uncc.edu>
2017-10-12 17:55:11 +0000

    Links now sort columns.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,6 @@
 module ApplicationHelper
     def sortable(column, title = nil)
         title ||= column.titleize
-        css_class = column == sort_column ? "hilite": nil
-        link_to title, {:sort => column}, {:class => css_class}
+        link_to title, {:sort => column}
     end
 end
